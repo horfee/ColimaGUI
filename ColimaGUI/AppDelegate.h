54d25df3b16feae136933096cf76553e0160fb81
Jean-Philippe Alexandre <horfee@gmail.com>
2022-02-24 13:16:37 +0100

    Initial Commit

---
+++ ColimaGUI/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ColimaGUI
+//
+//  Created by Jean-Philippe Alexandre on 24/02/2022.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ColimaGUI/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  ColimaGUI
+//
+//  Created by Jean-Philippe Alexandre on 24/02/2022.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
